e42c168034f044a7ea9f15632a1b8757ddf9b705
ninhtq97 <tranquangninh97@gmail.com>
2023-11-02 17:21:25 +0700

    [Fix] component Button

--- dist/components/Button/index.d.ts
+++ dist/components/Button/index.d.ts
@@ -1,14 +1,13 @@
 /// <reference types="react" />
-import { Center, End, Start } from 'types';
-type ButtonLoadingPosition = Start | End | Center;
 type ButtonVariant = 'filled' | 'outlined';
 type ButtonColor = 'primary' | 'secondary' | 'success' | 'error' | 'info' | 'warning';
+type ButtonSize = 'sm' | 'md' | 'lg';
 type Props = {
     loading?: boolean;
-    loadingPosition?: ButtonLoadingPosition;
     loadingIndicator?: string;
     variant?: ButtonVariant;
     color?: ButtonColor;
+    size?: ButtonSize;
 } & React.ComponentProps<'button'>;
 declare const Button: import("react").ForwardRefExoticComponent<Omit<Props, "ref"> & import("react").RefAttributes<HTMLButtonElement>>;
 export default Button;

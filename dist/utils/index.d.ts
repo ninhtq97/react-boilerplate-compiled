d7d4bfe055a2cb8abb5fd973ca9ce367d37447a0
ninhtq97 <tranquangninh97@gmail.com>
2024-03-07 16:18:59 +0700

    [Fix] datepicker

--- dist/utils/index.d.ts
+++ dist/utils/index.d.ts
@@ -1,9 +1,11 @@
+import { ClassValue } from 'clsx';
 export { default as api } from './api';
 export * from './array';
 export * from './date';
 export * from './error';
 export * from './storage';
 export * from './string';
+export declare function cn(...inputs: ClassValue[]): string;
 export declare const delay: (ms: number) => Promise<unknown>;
 export declare const excludeEmptyValue: <T extends {}>(obj: T) => T;
 export declare const recursiveRoutes: (routes: Record<string, any>, parentPath?: string) => Record<string, any>;

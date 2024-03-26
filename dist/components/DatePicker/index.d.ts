761e31c6cad81af9de0f4046b393ff20d1c348d4
ninhtq97 <tranquangninh97@gmail.com>
2024-03-26 09:55:34 +0700

    [Fix] css datepicker

--- dist/components/DatePicker/index.d.ts
+++ dist/components/DatePicker/index.d.ts
@@ -1,6 +1,8 @@
 /// <reference types="react" />
 import Datepicker from 'react-tailwindcss-datepicker';
 type Props = {
+    floating?: boolean;
+    label?: string;
     error?: boolean;
     helperText?: string;
 } & React.ComponentProps<typeof Datepicker>;
